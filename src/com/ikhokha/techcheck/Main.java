2ab2fe3d8aa585bb7d3925bd51c7e9bea8085e2d
karel <karel@swing.dev>
2022-10-04 13:46:53 +0200

    Fixed bug with reports only showing the last days results and added a null check for the file path.

--- src/com/ikhokha/techcheck/Main.java
+++ src/com/ikhokha/techcheck/Main.java
@@ -6,36 +6,42 @@ import java.util.Map;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		Map<String, Integer> totalResults = new HashMap<>();
-				
-		File docPath = new File("docs");
-		File[] commentFiles = docPath.listFiles((d, n) -> n.endsWith(".txt"));
-		
-		for (File commentFile : commentFiles) {
-			
-			CommentAnalyzer commentAnalyzer = new CommentAnalyzer(commentFile);
-			Map<String, Integer> fileResults = commentAnalyzer.analyze();
-			addReportResults(fileResults, totalResults);
-						
-		}
-		
-		System.out.println("RESULTS\n=======");
-		totalResults.forEach((k,v) -> System.out.println(k + " : " + v));
-	}
-	
-	/**
-	 * This method adds the result counts from a source map to the target map 
-	 * @param source the source map
-	 * @param target the target map
-	 */
-	private static void addReportResults(Map<String, Integer> source, Map<String, Integer> target) {
+    public static void main(String[] args) {
 
-		for (Map.Entry<String, Integer> entry : source.entrySet()) {
-			target.put(entry.getKey(), entry.getValue());
-		}
-		
-	}
+        Map<String, Integer> totalResults = new HashMap<>();
+
+        File docPath = new File("docs");
+        File[] commentFiles = docPath.listFiles((d, n) -> n.endsWith(".txt"));
+
+        if (commentFiles == null) {
+            System.out.println("File not found: " + docPath.getAbsolutePath());
+            return;
+        }
+
+        for (File commentFile : commentFiles) {
+
+            CommentAnalyzer commentAnalyzer = new CommentAnalyzer(commentFile);
+            Map<String, Integer> fileResults = commentAnalyzer.analyze();
+            addReportResults(fileResults, totalResults);
+
+        }
+
+        System.out.println("RESULTS\n=======");
+        totalResults.forEach((k, v) -> System.out.println(k + " : " + v));
+    }
+
+    /**
+     * This method adds the result counts from a source map to the target map
+     *
+     * @param source the source map
+     * @param target the target map
+     */
+    private static void addReportResults(Map<String, Integer> source, Map<String, Integer> target) {
+
+        for (Map.Entry<String, Integer> entry : source.entrySet()) {
+            target.merge(entry.getKey(), entry.getValue(), Integer::sum);
+        }
+
+    }
 
 }
